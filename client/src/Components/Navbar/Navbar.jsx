2560a3faac9111ecea253922139b8bfe22c744ae
Hirak7024 <hirakjyotidas2808000@gmail.com>
2024-03-21 13:17:45 +0530

    first commit

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@ export default function Navbar() {
     const navigate = useNavigate();
     const handleLogout = () => {
         localStorage.removeItem("authToken");
+        localStorage.removeItem("studentDetails");
         navigate("/");
         setUserData(null);
         toast.success("Logged Out")
